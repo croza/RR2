58b9e594c9f428f8169fa5d74a6508abc81bb142
croza <crozzabbz@gmail.com>
2011-06-16 17:10:06 +0100

    Edited the map converter, needs to be hex (0-9 can only have 10 map types)

--- tools/mapConverter.py
+++ tools/mapConverter.py
@@ -1,15 +1,20 @@
 # TBC add .cfg for the map converter
 #
 # Only to be used with maps made with Cyrem's map creator
+#
+# With thanks to DrakeMag from python-forum.org
 
 import os
 import sys
+
+import struct
 
 directory = raw_input("Please enter the directory (folder) of the map wished to be converted (e.g. 'map/'): ")
 X = int(raw_input("Please enter the width of the map (note that it is one more than what you put into the creator's size to be): "))
 Y = int(raw_input("Please enter the height of the map (same as with the width, one more than what you think it is: "))
 
-mapFiles = ['Surf.map', 'Cror.map', 'Dugg.map', 'Emrg.map', 'Erod.map', 'Fall.map', 'High.map', 'Path.map', 'Surf.map', 'Tuto.map']
+#mapFiles = ['Cror.map', 'Dugg.map', 'Emrg.map', 'Erod.map', 'Fall.map', 'High.map', 'Path.map', 'Surf.map', 'Tuto.map']
+mapFiles = ['Surf.map']
 otherFiles = ['ObjectList.ol']
 
 def ifExist(file2):
@@ -20,6 +25,14 @@ def ifExist(file2):
 		
 		
 def clipFile(file2):
+	def to_binary(hex_string): # hex_string being hex(int); it will be dealt with here
+		hex_string = hex_string[2:]
+		if (len(hex_string) == 0):
+			hex_string = '0'+hex_string
+			
+		ints = [int(hex_string[i:i+2], 16) for i in range(0,len(hex_string),2)]
+		return struct.pack('B' * len(ints), *ints)
+	
 	tempFile = open(directory+file2)
 	tempData = tempFile.read()
 
@@ -41,7 +54,9 @@ def clipFile(file2):
 			if (i % X == 0):
 				pass
 			else:
-				endList.append(int('0x'+tempData[i].encode("hex"), 0))
+				something = to_binary(hex(int('0x'+tempData[i].encode("hex"), 0)))
+				endList.append(something)
+				#endList.append(int('0x'+tempData[i].encode("hex"), 0))
 	
 	for value in endList:
 		endFile = endFile+str(value)
